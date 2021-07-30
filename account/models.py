bf3e483abe637944c98207037640e0cc9a920a9e
sherzodbek <9669470@gmail.com>
2021-07-30 18:21:41 +0500

    some changes

--- account/models.py
+++ account/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    PROFESSOR = 1
+    STUDENT = 2
+    ROLE_CHOICES = (
+        (PROFESSOR, 'Professor'),
+        (STUDENT, 'Student'),
+    )
+
+    email = models.EmailField(_('email address'), blank=True, unique=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=False, null=True)
+
+    USERNAME_FIELD = 'email'
