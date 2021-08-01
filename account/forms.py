1a0eb85a9747d06d026599504b66a5918b64e357
sherzodbek <9669470@gmail.com>
2021-08-01 16:19:05 +0500

    DivErrorList class created and added to SignUpForm form class

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-
+from django.forms.utils import ErrorList, format_html, format_html_join
 from account import models as app_models
+
+
+class DivErrorList(ErrorList):
+    def __init__(self, initlist=None, error_class='dsdsd'):
+        super().__init__(initlist, error_class)
+
+    def __str__(self):
+        return self.as_divs()
+
+    def as_divs(self):
+        if not self:
+            return ''
+        return format_html(
+            '<div class="{}">{}</div>',
+            self.error_class,
+            format_html_join('', '<div class="error">{}</div>', ((e,) for e in self))
+        )
 
 
 class SignInForm(forms.Form):
@@ -19,6 +36,11 @@ class SignInForm(forms.Form):
 
 
 class SignUpForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        kwargs_new = {'error_class': DivErrorList}
+        kwargs_new.update(kwargs)
+        super(SignUpForm, self).__init__(*args, **kwargs_new)
+
     # first_name = forms.CharField(max_length=30, required=True, help_text='Optional.')
     # last_name = forms.CharField(max_length=30, required=True, help_text='Optional.')
     # email = forms.EmailField(max_length=30, required=True, help_text='Required. Inform a valid email address.')
