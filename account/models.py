3f7ae6ae6147bc45b899d4c880cf21e760b48541
sherkhan7 <9669470@gmail.com>
2021-08-06 01:20:49 +0500

    some changes

--- account/models.py
+++ account/models.py
@@ -8,7 +8,6 @@ class User(AbstractUser):
     PROFESSOR = 1
     STUDENT = 2
     ROLE_CHOICES = (
-        (None, '----'),
         (PROFESSOR, 'Professor'),
         (STUDENT, 'Student'),
     )
@@ -16,7 +15,7 @@ class User(AbstractUser):
     first_name = models.CharField(_('first name'), max_length=150)
     last_name = models.CharField(_('last name'), max_length=150)
     email = models.EmailField(_('email address'), unique=True)
-    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True)
+    role = models.SmallIntegerField(choices=ROLE_CHOICES)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
